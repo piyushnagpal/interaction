3ae5000d9027702ad3a92db8813b0384b0147413
piyushnagpal <piyushngpal@gmail.com>
2016-09-24 14:23:13 +0530

    commit

---
+++ js/scripts.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+//console.log('document is ready');
+// I am going to write a code that makes a
+//div change color when it is clicked
+$('#click').click(function(){
+
+console.log('I clicked the div');
+
+$(".ball").css("background","pink");
+
+
+
+
+});
+
+});
